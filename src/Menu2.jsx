95902ebb995c56a6c5f39bbf683f683a6cb0715b
safahasnaoui22 <safadkhili22@gmail.com>
2024-09-26 17:23:45 +0100

    first commit

--- src/Menu2.jsx
+++ src/Menu2.jsx
@@ -8,10 +8,10 @@ function Menu2() {
         <h1 className="hmenu">Our Menu</h1>
         <div className="container">
           <div className="btn">
-            <a href="/M9.pdf" download="M9.pdf">Italian Menu</a>
+            <a href="/Menuitalien3.pdf" download="Menuitalien3.pdf">Italian Menu</a>
           </div>
           <div className="btn">
-            <a href="/menutunisienne.pdf" download="menutunisienne.pdf">Tunisian Menu</a>
+            <a href="/tones.pdf" download="tones.pdf">Tunisian Menu</a>
           </div>
         </div>
       </div>
